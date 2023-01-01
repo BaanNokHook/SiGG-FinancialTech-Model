8008ec0b166a0197e2d67cd0ac3602b98ea8d168
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-01 16:48:42 +0700

    SiGG-FinancialTech-Model-Quater-Common-UserSerializeModel

---
+++ Common/UserSerializeModel.cs
@@ -0,0 +1,22 @@
+namespace GM.Model.Common
+{
+    
+    public class UserSerializeModel  
+    {
+      public string UserId       { get; set; }   
+      public string UserName     { get; set; }  
+      public string UserEngName  { get; set; }
+      //public string UserThaiName { get; set; }   
+      public string TraderId { get; set; }   
+      public string DeskBookId { get; set; }
+      public string DeskBookName { get; set; }
+      public string DeskGroupId { get; set; }
+      public string DeskGroupName { get; set; }
+      public string Port { get; set; }
+      public string PortName { get; set; }
+      public string RoleId { get; set; }
+      public string RoleName { get; set; }
+      public string Token { get; set; }
+      //public AccessMenu MenuActive { get; set; }
+    }
+}
