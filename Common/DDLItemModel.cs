303f54be674c27f943210242c9eb465e6235716e
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-01 15:35:55 +0700

    SiGG-FinancialTech-Model-Quater-Common-DDLItemModel

---
+++ Common/DDLItemModel.cs
@@ -0,0 +1,12 @@
+namespace GM.Model.Common
+{
+  
+   public class DDLItemModel 
+   {
+      public string Text         { get; set; }  
+      public object Value        { get; set; }   
+      public object Value2       { get; set; }  
+      public object Value3       { get; set; }  
+      public object Value4       { get; set; }   
+      public string errormessage { get; set; }  
+   }
