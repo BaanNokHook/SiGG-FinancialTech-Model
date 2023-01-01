0bfbc698d54323a6048def27ba7aa5d1da9e4849
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-01 16:04:14 +0700

    SiGG-FinancialTech-Model-Quater-Common-LdapUserModel

---
+++ Common/LdapUserModel.cs
@@ -0,0 +1,56 @@
+namespace GM.Model.Common
+{
+    public class LdapUserModel
+    {
+        public string mailmessagestore { get; set; }
+        public string givenname { get; set; }
+        public string mailautoreplymode { get; set; }
+        public string inetuserstatus { get; set; }
+        public string preferredlanguage { get; set; }
+        public string personaltitle { get; set; }
+        public string citizenid { get; set; }
+        public string psincludeingab { get; set; }
+        public string icscalendar { get; set; }
+        public string lvldesc { get; set; }
+        public string thailastname { get; set; }
+        public string maildeliveryoption { get; set; }
+        public string objectclass { get; set; }
+        public string thaifirstname { get; set; }
+        public string rank { get; set; }
+        public string titlecode { get; set; }
+        public string mailuserstatus { get; set; }
+        public string icsdwphost { get; set; }
+        public string mail { get; set; }
+        public string sn { get; set; }
+        public string mailmsgquota { get; set; }
+        public string mailallowedserviceaccess { get; set; }
+        public string icsstatus { get; set; }
+        public string locationdesc { get; set; }
+        public string adspath { get; set; }
+        public string departmentcode { get; set; }
+        public string cn { get; set; }
+        public string loginname { get; set; }
+        public string icscalendarowned { get; set; }
+        public string icstimezone { get; set; }
+        public string inetcos { get; set; }
+        public string lvlno { get; set; }
+        public string mailquota { get; set; }
+        public string title { get; set; }
+        public string userpassword { get; set; }
+        public string postaladdress { get; set; }
+        public string businesscategory { get; set; }
+        public string mailsmtpsubmitchannel { get; set; }
+        public string locationcode { get; set; }
+        public string uid { get; set; }
+        public string proxystatus { get; set; }
+        public string icsfirstday { get; set; }
+        public string telephonenumber { get; set; }
+        public string thaipersonaltitle { get; set; }
+        public string kcsbranchcode { get; set; }
+        public string mailhost { get; set; }
+        public string userstatus { get; set; }
+        public string maildeferprocessing { get; set; }
+        public string mailalternateaddress { get; set; }
+        public string rankcode { get; set; }
+    }
+}
