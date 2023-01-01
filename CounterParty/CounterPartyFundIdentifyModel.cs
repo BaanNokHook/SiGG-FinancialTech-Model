c542c52e614d3af9380d313c704f9326bc79a88b
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-01 19:51:12 +0700

    SiGG-FinancialTech-Model-Quater-CounterParty-CounterPartyFundIdentifyModel

---
+++ CounterParty/CounterPartyFundIdentifyModel.cs
@@ -0,0 +1,72 @@
+using GM.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GM.Model.CounterParty
+{
+    public class CounterPartyFundIdentifyModel   
+    {
+        public string rowstatus { get; set; }   
+        public string unique_name { get; set; }   
+
+        public string identify_type_text { get; set; }   
+
+        [Required]  
+        public int? fund_id { get; set; }   
+
+        // [Required]   
+        public int? counter_party_id { get; set; }   
+
+        [Required]   
+        [Display(Name = "Unique")]   
+        [StringLength(6)]  
+        public string unique_id { get; set; }  
+        
+        [Display(Name = "Identify No")]   
+        [StringLength(50)]
+        public string identify_no { get; set; }  
+
+        [Display(Name = "Reg Bus Name(Th)")]   
+        [StringLength(255)]   
+        public string reg_bus_tname { get; set; }   
+
+        [Display(Name = "Reg Bus Name(Eng)")]
+        [StringLength(255)]
+        public string reg_bus_ename { get; set; }
+
+        [Display(Name = "Juristic Reg Date")]
+        public string juris_reg_date { get; set; }
+
+        [Display(Name = "Identify Type")]
+        [StringLength(255)]
+        public string identify_type { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Citizen ID")]
+        public string citizen_id { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Tax ID")]
+        public string tax_id { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Password")]
+        public string password_Number { get; set; }
+
+        public DateTime? create_date { get; set; }
+
+        [StringLength(255)]
+        public string create_by { get; set; }
+
+        public Nullable<DateTime> update_date { get; set; }
+
+        [StringLength(255)]
+        public string update_by { get; set; }
+
+        public virtual PagingModel paging { get; set; }
+
+        public virtual List<OrderByModel> ordersby { get; set; }
+    }
+
+}
