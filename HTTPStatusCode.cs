fbc7c28fbe7d7e38c3e0616ab51de1f709dd08ee
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-02 11:05:55 +0700

    SiGG-FinancialTech-Model-Quater-HTTPStatusCode

---
+++ HTTPStatusCode.cs
@@ -0,0 +1,20 @@
+﻿namespace GM.Model
+{
+    public enum HTTPStatusCode
+    {
+        OK = 200,
+        Created = 201,
+        NoContent = 204,
+        BadRequest = 400,
+        Unauthorized = 401,
+        Forbidden = 403,
+        NotFound = 404,
+        MethodNotAllowed = 405,
+        Conflict = 409,
+        LengthRequired = 411,
+        PreconditionFailed = 412,
+        TooManyRequests = 429,
+        InternalServerError = 500,
+        ServiceUnavailable = 503
+    }
+}
