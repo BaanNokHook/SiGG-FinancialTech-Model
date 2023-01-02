439fa63888145b6cc7386676df90dfe74a1cca8a
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-02 10:00:19 +0700

    SiGG-FinancialTech-Model-Quater-StockReconcile

---
+++ StockReconcile/StockReconcileImportModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GM.Model.StockReconcile
+{
+    public class StockReconcileImportModel
+    {
+        public string import_id { get; set; }
+        public string filename { get; set; }
+        public DateTime asOfDate { get; set; }
+        public string participant { get; set; }
+        public string accno { get; set; }
+        public string instrumentCode { get; set; }
+        public string isincode { get; set; }
+        public decimal? unit { get; set; }
+        public decimal? pending_withdrawal { get; set; }
+        public decimal? pending_deposit { get; set; }
+        public decimal? pending_sec { get; set; }
+        public string broker { get; set; }
+        public DateTime? create_date { get; set; }
+        public string create_by { get; set; }
+        public DateTime? update_date { get; set; }
+        public string update_by { get; set; }
+    }
+}
---
+++ StockReconcile/StockReconcileModel.cs
@@ -0,0 +1,27 @@
+﻿using GM.Model.Common;
+using System;
+using System.Collections.Generic;
+
+namespace GM.Model.StockReconcile
+{
+    public class StockReconcileModel
+    {
+        public int RowNumber { get; set; }
+        public DateTime? as_of_date { get; set; }
+        public int? instrument_id { get; set; }
+        public string instrument_code { get; set; }
+        public decimal afs_unit { get; set; }
+        public decimal htm_unit { get; set; }
+        public decimal memo_bnk_unit { get; set; }
+        public decimal memo_trd_unit { get; set; }
+        public decimal total_unit { get; set; }
+        public decimal outstanding_unit { get; set; }
+        public decimal obligate_unit { get; set; }
+        public decimal diff_unit { get; set; }
+        public string remark { get; set; }
+        public string import_id { get; set; }
+        public string recorded_by { get; set; }
+        public virtual PagingModel Paging { get; set; }
+        public virtual List<OrderByModel> OrdersBy { get; set; }
+    }
+}
