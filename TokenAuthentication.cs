50886508c91217e3d5a96b0fb80cadee8675616f
SiGGFedCJKP <baannokhooksg@gmail.com>
2023-01-02 11:51:23 +0700

    SiGG-FinancialTech-Model-Quater-TokenAuthentication

---
+++ TokenAuthentication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GM.Model
+{
+    public class TokenAuthentication
+    {
+        public String Audience { get; set; }
+        public String SecretKey { get; set; }
+        public String Path { get; set; }
+    }
+}
